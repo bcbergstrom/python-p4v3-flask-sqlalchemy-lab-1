ce1cce643e785c819ac445e7206d24bde151e2e8
Benton Bergstrom <bbergstrom0524@gmail.com>
2024-08-05 16:04:36 -0600

    final commit

--- server/app.py
+++ server/app.py
@@ -21,7 +21,21 @@ def index():
     return make_response(body, 200)
 
 # Add views here
-
-
+@app.route('/earthquakes/<int:id>')
+def route_id(id):
+    query_first = Earthquake.query.filter_by(id=id).first()
+    if query_first is None:
+        return make_response({'message': 'Earthquake {} not found.'.format(id)}, 404)
+    return query_first.to_dict()    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def route_magnitude(mag):
+    all_e = Earthquake.query.filter(Earthquake.magnitude >= mag).all()
+    return_list = []
+    count = 0
+    for i in all_e:
+        return_list.append(i.to_dict())
+        count += 1
+    return {'count': count, 'quakes': return_list} 
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
